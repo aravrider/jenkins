ceaddca2aac781f96ab4db30461035f1fb10dcae
Aman Kant Mahto <125489955+aravrider@users.noreply.github.com>
2024-03-23 21:20:37 +0530

    Update emailed.py

--- emailed.py
+++ emailed.py
@@ -1,17 +1,23 @@
+
 import smtplib
-my_mail="aravrider3@gmail.com"
-passcode="hibjljyoybtatmuz"
 
-connection= smtplib.SMTP("smtp.gmail.com")
+my_mail = "aravrider3@gmail.com"
+passcode = "hibjljyoybtatmuz"
+
+connection = smtplib.SMTP("smtp.gmail.com")
 connection.starttls()
-connection.login(user=my_mail,password=passcode)
+connection.login(user=my_mail, password=passcode)
 
-mail_content=" Subject: trip on this weekend: \n\n hey aao kbhi haweli pe"
+subject = "Trip on this weekend"
+body = "Hey, aao kbhi haweli pe"
+
+mail_content = f"Subject: {subject}\n\n{body}"
 
 try:
     connection.sendmail(from_addr=my_mail, to_addrs="amanmahto669@gmail.com", msg=mail_content)
+    print("Email sent successfully!")
 
 except Exception as e:
-    print("something went wrong",e)
+    print("Something went wrong:", e)
 
 connection.close()
